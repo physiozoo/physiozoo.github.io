eb3c1a5bd0c42de80a0a5c37c33b6dbf04be8544
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-05-21 10:38:13 +0300

    docs: Added logo

--- docs/conf.py
+++ docs/conf.py
@@ -108,6 +108,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_logo = "_static/logo.png"
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
